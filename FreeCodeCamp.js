b72ac048ab63b51734dc06482c74c27a91ac255d
LizzieLaShark <lizzie.s.sullivan@gmail.com>
2015-11-23 14:48:23 +1300

    Create FreeCodeCamp.js

---
+++ FreeCodeCamp.js
@@ -0,0 +1,40 @@
+var ourName = "Free Code Camp";
+var myName = "Lizzie";
+
+var firstName = "Alan";
+var lastName = "Turing";
+var myFirstName = "Lizzie";
+var myLastName = "Sullivan";
+
+var firstNameLength = 0;
+var lastNameLength = 0;
+var firstName = "Ada";
+
+firstNameLength = firstName.length;
+
+var lastName = "Lovelace";
+
+// Only change code below this line.
+
+lastNameLength = lastName.length;
+
+var firstLetterOfFirstName = "[0]";
+var firstLetterOfLastName = "[0]";
+
+var firstName = "Ada";
+
+firstLetterOfFirstName = firstName[0];
+
+var lastName = "Lovelace";
+
+firstLetterOfLastName = lastName[0];
+var firstName = "Ada";
+
+var secondLetterOfFirstName = firstName[1];
+
+var lastName = "Lovelace";
+
+var thirdLetterOfLastName = lastName[2];
+
+
+
